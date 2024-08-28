8b0c3873d6155cf7a5952222aadd4f5f0111cf9f
GabrielFFSantos <gabrielfilipef@hotmail.com>
2024-08-28 16:21:10 -0300

    Feature Page home and layout

---
+++ src/app/Home/home.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Home from "./page";
+
+//Declaração de "coleção ou principal"
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+} as Meta;
+
+//Declaração das variantes do obj. (se existir)
+
+export const Primary: StoryObj = {};
---
+++ src/app/Home/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.css";
+
+export default function Home() {
+  return <main className={`${styles.content}`}>HomePage</main>;
+}
